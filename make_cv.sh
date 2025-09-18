5909f7d4b73faf375dcbdb24991618b14eaedfaf
abo <abo>
2025-09-18 09:37:59 -0400

    HACK: sed to change geometry

--- make_cv.sh
+++ make_cv.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # pandoc -s cv_en_john_doe.md -o cv_en_john_doe.docx
-python md_to_tex.py cv.md english
+python3 md_to_tex.py cv.md english
+sed -i 's/\[scale=0.75\]{geometry}/\[scale=0.85\]{geometry}/' cv.tex
 pdflatex cv.tex
